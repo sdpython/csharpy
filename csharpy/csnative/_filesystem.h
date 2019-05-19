99b20f97362a3aa946affb4e74cf833d630dfb75
xavier dupré <xavier.dupre@gmail.com>
2019-05-20 00:25:36 +0200

    Update _filesystem.h

--- csharpy/csnative/_filesystem.h
+++ csharpy/csnative/_filesystem.h
@@ -1,6 +1,7 @@
 #ifndef __cplusplus
   #error C++ is required
 #else
+    #include <iostream>
     #if __cplusplus < 201703L
     #include <experimental/filesystem>
     namespace fs = std::experimental::filesystem;
