40615027d0369a0523b2a0c89aaf4821b3163df1
xavier dupré <xavier.dupre@gmail.com>
2019-09-08 14:48:08 +0200

    Add a test to check an assumption

--- cscode/DynamicCScore/BuildHelper.cs
+++ cscode/DynamicCScore/BuildHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Text;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
 
 namespace DynamicCS
@@ -25,6 +27,24 @@ namespace DynamicCS
                     res.Add(name);
             }
             return res.ToArray();
+        }
+
+        public static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
         }
     }
 }
---
+++ cscode/TestCSharpyCore/TestBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DynamicCS;
+using CSharPyExtension;
+
+
+namespace TestCSharpyCore
+{
+    [TestClass]
+    public class TestBuilder
+    {
+        [TestMethod]
+        public void TestSHA256()
+        {
+            var raw = "abc";
+            var cmp = BuildHelper.ComputeSha256Hash(raw);
+            Assert.IsNotNull(cmp);
+            Assert.IsTrue(cmp.Length >= 10);
+        }
+    }
+}
+
